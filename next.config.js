3089fba87cdee9d2b580d5d039a82cb6eca20734
nftname <nftnamemarket@gmail.com>
2025-12-29 15:58:54 +0000

    Fix config and Clean Build

--- next.config.js
+++ next.config.js
@@ -1,27 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   typescript: {
     ignoreBuildErrors: true,
-  },
-  serverExternalPackages: ["pino", "pino-pretty", "thread-stream"],
-  turbopack: {},
-  webpack: (config) => {
-    config.externals.push("pino-pretty", "lokijs", "encoding");
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-      net: false,
-      tls: false,
-      crypto: false,
-      stream: false,
-      url: false,
-      zlib: false,
-      http: false,
-      https: false,
-    };
-    return config;
-  },
+  }
 };
 
 module.exports = nextConfig;
