821e1c302d8d3db28ed086d86723999e08c03e1d
nftname <nftnamemarket@gmail.com>
2026-01-11 18:30:58 +0000

    Update project files

--- src/components/ContentProtection.tsx
+++ src/components/ContentProtection.tsx
@@ -4,24 +4,16 @@ import { useEffect } from 'react';
 
 export default function ContentProtection() {
   useEffect(() => {
-    // Disable copy
+    // Disable copy only
     const handleCopy = (e: ClipboardEvent) => {
       e.preventDefault();
       return false;
     };
 
-    // Disable cut
-    const handleCut = (e: ClipboardEvent) => {
-      e.preventDefault();
-      return false;
-    };
-
     document.addEventListener('copy', handleCopy);
-    document.addEventListener('cut', handleCut);
 
     return () => {
       document.removeEventListener('copy', handleCopy);
-      document.removeEventListener('cut', handleCut);
     };
   }, []);
 
