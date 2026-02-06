9fb1be5551a3db75b60150e4bf0bb1e9a0dc47fa
Mohamed <nftnamemarket@gmail.com>
2026-02-06 19:06:46 +0200

    Update fixed viewport and Alayout fixes

--- src/app/asset/[id]/page.tsx
+++ src/app/asset/[id]/page.tsx
@@ -1074,7 +1074,7 @@ function AssetPage() {
                                                         <table className="table mb-0" style={{ backgroundColor: 'transparent', color: '#fff', borderCollapse: 'separate', borderSpacing: '0' }}>
                                                             <thead>
                                                         <tr>
-                                                            <th style={{ backgroundColor: 'transparent', color: '#8a939b', fontWeight: 'normal', fontSize: '12px', borderBottom: '1px solid #2d2d2d', padding: '0 0 10px 0', width: '15%' }}>WPOL</th>
+                                                            <th style={{ backgroundColor: 'transparent', color: '#8a939b', fontWeight: 'normal', fontSize: '12px', borderBottom: '1px solid #2d2d2d', padding: '0 0 10px 0', width: '15%' }}>$</th>
                                                             <th style={{ backgroundColor: 'transparent', color: '#8a939b', fontWeight: 'normal', fontSize: '12px', borderBottom: '1px solid #2d2d2d', padding: '0 0 10px 0', width: '30%' }}>From</th>
                                                             <th style={{ backgroundColor: 'transparent', color: '#8a939b', fontWeight: 'normal', fontSize: '12px', borderBottom: '1px solid #2d2d2d', padding: '0 0 10px 0', width: '30%' }}>To</th>
                                                             <th style={{ backgroundColor: 'transparent', color: '#8a939b', fontWeight: 'normal', fontSize: '12px', borderBottom: '1px solid #2d2d2d', padding: '0 0 10px 0', width: '12%' }}>Status</th>
