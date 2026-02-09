b252ab18c7b12dfacf336d4bdc64869fa4d8f814
Mohamed <nftnamemarket@gmail.com>
2026-02-09 18:07:28 +0200

    Update fixed viewport and Alayout fixes

--- src/app/demopage/page.tsx
+++ src/app/demopage/page.tsx
@@ -92,8 +92,8 @@ export default function DemoProfilePage() {
             width: 100%;
             height: 30vh; 
             height: auto;          /* الارتفاع أوتوماتيكي */
-             aspect-ratio: 1.3;  /* النسبة الأصلية للصورة (3360/1280) تضمن عدم القص */
-             max-height: 250px;      /* حد أقصى للارتفاع */
+            aspect-ratio: 3.3 / 1;  /* النسبة الأصلية للصورة (3360/1280) تضمن عدم القص */
+            max-height: 250px;      /* حد أقصى للارتفاع */
             position: relative;
             background-color: #000;
             overflow: hidden;
