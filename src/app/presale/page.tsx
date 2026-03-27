84c4395f2ee24933a872c397b2a9bf1309ccd4c1
Mohamed <nftnamemarket@gmail.com>
2026-03-27 22:10:13 +0300

    Update fixed viewport and Alayout fixes

--- src/app/presale/page.tsx
+++ src/app/presale/page.tsx
@@ -573,7 +573,7 @@ export default function PresalePage() {
         </div>
         <div style={{ textAlign: 'right' }}>
           <div style={{ color: '#9ea9a9', fontSize: '16px', marginBottom: '2px' }}>
-            of <span style={{ color: '#fff' }}>$1,050,000</span> Goal
+            of <span style={{ color: '#fff' }}>$1,050,000</span>
           </div>
         </div>
       </div>
