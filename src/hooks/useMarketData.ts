99d1ac77f5e0c268b52f5541d6eb474102bb961d
Mohamed <nftnamemarket@gmail.com>
2026-03-29 01:48:22 +0300

    Update fixed viewport and Alayout fixes

--- src/hooks/useMarketData.ts
+++ src/hooks/useMarketData.ts
@@ -150,9 +150,9 @@ export function useMarketData(timeFilter: string = 'All') {
                     const offersCount = Number(dbRecord.offerscount) || 0;
                     const itemVolume = Number(dbRecord.volume) || 0;
 
-                    const volScore = maxVolume > 0 ? (itemVolume / maxVolume) * 35 : 0;
-                    const salesScore = maxSales > 0 ? (salesCount / maxSales) * 25 : 0;
-                    const convictionScorePercent = maxConviction > 0 ? (organicPoints / maxConviction) * 25 : 0;
+                    const volScore = maxVolume > 0 ? (itemVolume / maxVolume) * 30 : 0;
+                    const salesScore = maxSales > 0 ? (salesCount / maxSales) * 20 : 0;
+                    const convictionScorePercent = maxConviction > 0 ? (organicPoints / maxConviction) * 35 : 0;
                     const offersScore = maxOffers > 0 ? (offersCount / maxOffers) * 15 : 0;
 
                     const trendingScore = volScore + salesScore + convictionScorePercent + offersScore;
